7d14b808712ff40f509f049d1d9354fbede95672
munir79 <jakirhossainmunir79@gmail.com>
2023-08-02 10:54:26 +0600

    dynamic route

--- src/Component/User.js
+++ src/Component/User.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import '../Style/User.css';
+import { Link } from 'react-router-dom';
 
 const User = (props) => {
   console.log(props);
-    const {name,email}=props.friend;
+    const {name,email,username,id}=props.friend;
     return (
-        <div>
+        <div className='user1'> 
             <h4> Name:{name}</h4>
             <h5> email:{email}</h5>
+            <h3> UserName: <Link to={`/friend/${id}`}>   {username}</Link>  </h3>
         </div>
     );
 };
